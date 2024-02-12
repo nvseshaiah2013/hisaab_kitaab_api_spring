8ba0a2ea57272ccebd8a042b8c4b92dbea0129e2
Rahul Ojha <rahulojha1097@gmail.com>
2024-02-12 14:45:10 +0530

    implemented getUsers by username prefix

--- src/main/java/com/kitaab/hisaab/ledger/service/impl/UserServiceImpl.java
+++ src/main/java/com/kitaab/hisaab/ledger/service/impl/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package com.kitaab.hisaab.ledger.service.impl;
 
 import com.kitaab.hisaab.ledger.dto.response.SuccessResponse;
+import com.kitaab.hisaab.ledger.entity.user.User;
+import com.kitaab.hisaab.ledger.repository.UserRepository;
 import com.kitaab.hisaab.ledger.service.JwtService;
 import com.kitaab.hisaab.ledger.service.UserService;
 import lombok.AllArgsConstructor;
@@ -11,6 +13,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -21,6 +24,8 @@ public class UserServiceImpl implements UserService {
     private AuthenticationManager authenticationManager;
 
     private JwtService jwtService;
+
+    private UserRepository userRepository;
 
     @Override
     public SuccessResponse login(String username, String password) {
@@ -54,6 +59,9 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public SuccessResponse getUsers(String usernamePrefix) {
-        return null;
+        log.info("Finding all Users starting with Username : {}",usernamePrefix);
+        List<User> users = userRepository.findByUsernameStartsWith(usernamePrefix);
+        log.info("Found {} users starting with username : {}",users.size(),usernamePrefix);
+        return new SuccessResponse(true, String.format("Found %d users",users.size()), users);
     }
 }
